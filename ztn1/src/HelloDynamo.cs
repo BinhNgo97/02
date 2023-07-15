36ed2267336a8d2b16e0f0d525cb77f639913cad
BinhNgo97 <104677276+BinhNgo97@users.noreply.github.com>
2023-07-15 13:20:13 +0700

    ztn1
    add new project

---
+++ ztn1/src/HelloDynamo.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ztn1
+{
+    /// <summary>
+    /// Hello World basic sample
+    /// </summary>
+    public static class HelloDynamo
+    {
+        /// <summary>
+        /// Prints a friendly greeting
+        /// </summary>
+        /// <param name="name">Name of the person to greet</param>
+        /// <returns>A greeting</returns>
+        public static string SayHello(string name)
+        {
+            return String.Format("Hello {0}!", name);
+        }
+    }
+}
